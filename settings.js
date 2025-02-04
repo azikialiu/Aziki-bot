a88ddb09a8403652d1b66f139a55aa7accfb5394
azikialiu <127680255+azikialiu@users.noreply.github.com>
2025-02-04 19:58:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'botaziki',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "aziki Bot",
+  botOwner: 'abdeali', // Your name
+  ownerNumber: '212772076581', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
